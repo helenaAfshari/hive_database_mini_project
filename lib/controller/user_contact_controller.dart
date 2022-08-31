3400929f2f9f677907770be83297ce4896c719ec
helenaAfshari <afshari.hln@gmail.com>
2022-08-31 18:50:09 +0430

    create class usercontactcontroller

--- lib/controller/user_contact_controller.dart
+++ lib/controller/user_contact_controller.dart
@@ -1,9 +1,23 @@
 
 
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:hive_database_mini_project/model/user_model.dart';
 
 class UserContactController extends GetxController{
 
     
+    TextEditingController nametextEditingController = TextEditingController();
+    TextEditingController phonetextEditingController = TextEditingController();
 
+    RxList <UserModel> usermodel = RxList();
+    
+
+
+
+    addToContactList(){
+         
+         
+
+    }
 }
