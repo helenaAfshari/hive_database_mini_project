fe11db869ad083a3a98e76b9c1f49909ab125481
helenaAfshari <afshari.hln@gmail.com>
2022-08-31 19:26:40 +0430

    addToContactList

--- lib/controller/user_contact_controller.dart
+++ lib/controller/user_contact_controller.dart
@@ -2,7 +2,9 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:hive_database_mini_project/constants/hive_keys.dart';
 import 'package:hive_database_mini_project/model/user_model.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class UserContactController extends GetxController{
 
@@ -10,14 +12,31 @@ class UserContactController extends GetxController{
     TextEditingController nametextEditingController = TextEditingController();
     TextEditingController phonetextEditingController = TextEditingController();
 
-    RxList <UserModel> usermodel = RxList();
+    RxList <UserModel> contactList = RxList();
     
 
 
 
-    addToContactList(){
+    addToContactList()async{
          
-         
+         //create Box
+        var box = await Hive.openBox(HiveFieldConstants.userContactListBox);
 
+        // Add controller in list
+        var user = UserModel(
+          name: nametextEditingController.text,
+           phone: phonetextEditingController.text);
+            
+            //user add in box
+            box.add(user);
+
+            box.close();
+
+            //read user from box 
+             box.values.forEach((element) {
+              UserModel userModel = element;
+              contactList.add(userModel);
+              print(userModel.name);
+             });
     }
 }
