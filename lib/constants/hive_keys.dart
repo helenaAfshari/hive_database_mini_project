430e85bb4600454f1213b0097f4900c3e98cd2f4
helenaAfshari <afshari.hln@gmail.com>
2022-08-31 19:00:49 +0430

    create HiveBox

---
+++ lib/constants/hive_keys.dart
@@ -0,0 +1,11 @@
+
+
+
+//Hive fixed keys are placed in this class
+class HiveFieldConstants{
+
+//Box Hive
+  static const String userContactListBox  = "userContactListBox"; 
+  static const String phoneContactListBox  = "userContactListBox";
+
+}
